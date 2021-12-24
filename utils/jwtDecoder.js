11e6b62dd517c8708e93b92ed49490810d55162f
Maurizio Bella <mauriziobella@gmail.com>
2021-12-23 20:50:16 -0500

    quick fix

--- utils/jwtDecoder.js
+++ utils/jwtDecoder.js
@@ -4,9 +4,9 @@ module.exports = (body) => {
   if (!body) {
     return new Error('invalid jwtdata');
   }
-  const verified = JWT.verify(body, process.env.JWT);
-  console.log('----------------------------------------------------------------');
-  console.log(verified);
+  // const verified = JWT.verify(body, process.env.JWT);
+  // console.log('----------------------------------------------------------------');
+  // console.log(verified);
   return JWT.verify(body.toString('utf8'), process.env.JWT, {
     algorithm: 'HS256',
   });
